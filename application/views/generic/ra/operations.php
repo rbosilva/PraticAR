1ca1d02af5ad47781d19d7b2e5713c4215bf0fef
rodrigo.barbosa <barbosa@tecnosistemas.com.br>
2017-11-08 14:07:15 -0200

    Ajustes nas dicas flutuantes das operações em AR

--- application/views/generic/ra/operations.php
+++ application/views/generic/ra/operations.php
@@ -8,13 +8,16 @@
    <b style='color: red;'>⨝</b> <b style='color: blue;'>(relA.col1 = relB.col1)</b> relB">⨝</a>
 <a aria-role="button" type="button" class="btn btn-default" title="Divisão (NORMAL / <b style='color: blue;'>PROJEÇÃO</b>)" data-operacao="÷"
    data-toggle="popover" data-content="relA <b style='color: red;'>÷</b> (relA.col = relB.col) relB<br>
-   <b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relA<b style='color: blue;'>)</b> <b style='color: red;'>÷</b> (relA.col = relB.col) relB">÷</a>
+   <b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relA<b style='color: blue;'>)</b> <b style='color: red;'>÷</b> (relA.col = relB.col) relB<br>
+   <b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relA <b style='color: red;'>÷</b> (relA.col = relB.col) relB<b style='color: blue;'>)</b>">÷</a>
 <a aria-role="button" type="button" class="btn btn-default" title="União" data-operacao="∪"
    data-toggle="popover" data-content="relA <b style='color: red;'>∪</b> relB">∪</a>
 <a aria-role="button" type="button" class="btn btn-default" title="Intersecção" data-operacao="∩"
-   data-toggle="popover" data-content="relA <b style='color: red;'>∩</b> relB">∩</a>
+   data-toggle="popover" data-content="<b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relA<b style='color: blue;'>)</b> <b style='color: red;'>∩</b>
+   <b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relB<b style='color: blue;'>)</b>">∩</a>
 <a aria-role="button" type="button" class="btn btn-default" title="Diferença" data-operacao="-"
-   data-toggle="popover" data-content="relA <b style='color: red;'>-</b> relB">-</a>
+   data-toggle="popover" data-content="<b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relA<b style='color: blue;'>)</b> <b style='color: red;'>-</b>
+   <b style='color: blue;'>π col</b> <b style='color: blue;'>(</b>relB<b style='color: blue;'>)</b>">-</a>
 <a aria-role="button" type="button" class="btn btn-default" title="Produto Cartesiano" data-operacao="X"
    data-toggle="popover" data-content="relA <b style='color: red;'>X</b> relB">X</a>
 <a aria-role="button" type="button" class="btn btn-default" title="Renomear (TABELA / <b style='color: blue;'>COLUNA(S)</b>)"
